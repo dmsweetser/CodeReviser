09ad68e6c8229b69a7a24db7c5e2c383cbdeebe8
Daniel Sweetser <37463869+dmsweetser@users.noreply.github.com>
2023-12-16 12:05:16 +0000

    Working towards an ever-expanding code generation

--- CodeReviser.py
+++ CodeReviser.py
@@ -7,6 +7,11 @@ import logging
 import re
 from llama_cpp import Llama
 import time
+import requests
+
+max_tokens = 16384
+file_extensions = ('.py', '.java', '.cpp', '.cs', '.cshtml', '.js')
+
 
 def archive_prior_results(output_directory, current_round):
     # Archive prior results before starting a new round
@@ -30,19 +35,20 @@ def archive_directory(source, destination):
 
 def setup_logging():
     # Configure logging to log to a timestamped file
-    log_filename = f"script_log_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.txt"
+    log_filename = f"script_log_{datetime.datetime.now().strftime('%Y%m%d')}.txt"
     logging.basicConfig(filename=log_filename, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def generate_code_revision(original_code, temperature):
-    # Generate code revision using llama_cpp library
+def generate_code_revision(original_code, output_directory,):
+    # Check if original_code is more than 40% of max_tokens
+    if len(original_code) > 0.7 * max_tokens:
+        return generate_file_revisions(original_code, output_directory)
+
     logging.info("Generating code revision.")
     
     try:
+        messages = [{"role": "system", "content": "Enhance the provided code by replacing placeholders or pseudocode with actual implementations. Utilize the same programming language as the original. Generate properly-escaped comments, including for potential new features. Present the revised code solely in markdown format. Here is the provided code: " + original_code}]
+        response = llama.create_chat_completion(messages=messages, temperature=1.0)
 
-        messages = [{"role": "system", "content": "Can you make this code better? If you see placeholders or pseudocode, please replace them with actual implementations. Please generate revisions in the same code language as the original. Please only respond with the revised code in markdown: " + original_code}]
-        response = llama.create_chat_completion(messages=messages, temperature=temperature)
-
-        # Log the question and response
         logging.info(f"Question: {messages[0]['content']}")
         logging.info(f"Response: {response}")
 
@@ -52,8 +58,65 @@ def generate_code_revision(original_code, temperature):
         logging.error(f"Error generating code revision: {str(e)}")
         return original_code  # Return original code in case of an error
 
-def process_file(input_path, output_path, temperature):
-    # Process a file by generating a code revision and extracting code blocks
+def generate_file_revisions(original_code, output_directory):
+    # Generate code revisions for individual files
+    logging.info("Generating file revisions.")
+
+    try:
+        messages = [{"role": "system", "content": "Refactor the provided code to enable separation into individual files. Each refactored part should be enclosed in markdown, representing an individual file. Maintain the original programming language. For each file, include appropriately escaped comments detailing the file's purpose, relevant features, and any shared interfaces. Please use the given code for refactoring: " + original_code}]
+        response = llama.create_chat_completion(messages=messages, temperature=1.0)
+
+        logging.info(f"Question: {messages[0]['content']}")
+        logging.info(f"Response: {response}")
+
+        # Extract file names and contents from the response
+        file_revisions = extract_file_revisions(response)
+        logging.info("Here are the file revisions:")
+        logging.info(file_revisions)
+        
+        return file_revisions
+
+    except Exception as e:
+        logging.error(f"Error generating file revisions: {str(e)}")
+        return [{"filename": "error_file.txt", "content": original_code}]
+
+def extract_file_revisions(response):
+    # Extract file contents from the response using backticks
+    file_revisions = []
+    pattern = re.compile(r'```.*?```', re.DOTALL)
+
+    matches = pattern.finditer(response['choices'][0]['message']['content'])
+
+    for index, match in enumerate(matches, start=1):
+        content = match.group(0)[3:-3].strip()  # Remove triple backticks and strip whitespaces
+        # Remove the first line, which would include the language name
+        content = content.split('\n', 1)
+        file_revisions.append({"filename": f"file_{index}", "content": content})
+
+    return file_revisions
+
+def save_file_revisions(file_revisions, output_directory, round_number, original_filename):
+    # Save the contents as files in the current round directory
+    round_output_directory = os.path.join(output_directory, f"round_{round_number}")
+
+    for index, revision in enumerate(file_revisions, start=1):
+        original_extension = os.path.splitext(original_filename)[1]
+        new_filename = f"{original_filename}_{index}{original_extension}"
+        new_filepath = os.path.join(round_output_directory, new_filename)
+
+        # Check if the content is a list (in case it's a code block with language info)
+        if isinstance(revision["content"], list):
+            # Join the list elements into a string
+            content_str = '\n'.join(revision["content"])
+        else:
+            content_str = revision["content"]
+
+        with open(new_filepath, 'w') as file:
+            file.write(content_str)
+
+        logging.info(f"File revision saved: {new_filepath}")
+
+def process_file(input_path, output_path, output_directory, round_number):
     logging.info(f"Processing file: {input_path}")
 
     try:
@@ -63,11 +126,19 @@ def process_file(input_path, output_path, temperature):
         # Convert original_code to a string
         original_code = str(original_code)
 
-        revised_response = generate_code_revision(original_code, temperature)
+        response = generate_code_revision(original_code, output_directory)
+
+        if type(response) == list:
+            # File revisions generated, save them
+            save_file_revisions(response, output_directory, round_number, os.path.basename(input_path))
+            # Remove the input file after processing
+            os.remove(input_path)
+            logging.info(f"Input file {input_path} removed after processing.")
+            return
 
         # Extract the revised code from the response inside the Markdown code block using regular expression
         pattern = re.compile(r'```.*?```', re.DOTALL)
-        match = pattern.search(revised_response['choices'][0]['message']['content'])
+        match = pattern.search(response['choices'][0]['message']['content'])
 
         if match:
             revised_code = match.group(0)[3:-3].strip()  # Remove the triple backticks and strip leading/trailing whitespaces
@@ -79,17 +150,21 @@ def process_file(input_path, output_path, temperature):
 
         else:
             # If no match is found, use the entire content
-            revised_code = revised_response['choices'][0]['message']['content'].strip()
+            revised_code = response['choices'][0]['message']['content'].strip()
 
         # Write the revised code to the output file
         with open(output_path, 'w') as file:
             file.write(revised_code)
 
+        # Save file revisions with the original filename
+        save_file_revisions([{"filename": os.path.basename(input_path), "content": revised_code}],
+                            output_directory, round_number, os.path.basename(input_path))
+
     except Exception as e:
         logging.error(f"Error processing file {input_path}: {str(e)}")
-        shutil.copyfile(input_path, output_path)  # Copy the original file in case of an error
+        shutil.copy2(input_path, output_path)
 
-def main(target_directory, output_directory, rounds, temperatures):
+def main(target_directory, output_directory, rounds):
 
     # Main function to process files in multiple rounds
     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
@@ -114,14 +189,11 @@ def main(target_directory, output_directory, rounds, temperatures):
         # Archive prior results
         archive_prior_results(output_directory, round_number)
 
-        # Use the specified temperature for this round
-        temperature = temperatures[round_number - 1]
-
         for root, dirs, files in os.walk(round_output_directory):
             for file in files:
                 file_path = os.path.join(root, file)
-                if file.lower().endswith(('.py', '.java', '.cpp', '.cs', '.cshtml', '.js')):
-                    process_file(file_path, file_path, temperature)
+                if file.lower().endswith(file_extensions):
+                    process_file(file_path, file_path, output_directory, round_number)
 
     logging.info("Script execution completed.")
 
@@ -145,9 +217,8 @@ if __name__ == "__main__":
     setup_logging()
     source_directory = "Source"
     output_directory = "Output"
-    rounds = 5
+    rounds = 100
     model_name = file_name
-    temperatures = [1.0, 1.0, 0.87, 1.0, 0.87]  # Specify temperatures for each round
     
     # Define llama.cpp parameters
     llama_params = {
@@ -172,12 +243,13 @@ if __name__ == "__main__":
         "top_k": 85,
         "repetition_penalty": 1.01,
         "typical_p": 0.68,
-        "tfs": 0.68
+        "tfs": 0.68,
+        "max_tokens": max_tokens
     }
     
     llama = Llama(model_name, **llama_params)
     
-    main(source_directory, output_directory, rounds + 1, temperatures)
+    main(source_directory, output_directory, rounds + 1)
         
     end_time = time.time()
     total_execution_time = end_time - start_time
